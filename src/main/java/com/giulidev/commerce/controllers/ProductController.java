0a5ac7db3734db5fcdeaec8c6c6a2566d88aa92d
Giuli Ilustre <ilustre.giuli@gmail.com>
2026-04-21 20:26:54 -0300

    Colocando atualizacao (update) de registro

--- src/main/java/com/giulidev/commerce/controllers/ProductController.java
+++ src/main/java/com/giulidev/commerce/controllers/ProductController.java
@@ -78,4 +78,14 @@ public class ProductController {
     }
     // ******************************************************************************************
 
+    // ATUALIZANDO REGISTRO
+    // ******************************************************************************************
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO productDTO) {
+        productDTO = productService.update(id, productDTO);
+        // Envio da reposta personalizada (OK) e envio do DTO no corpo da resposta
+        return ResponseEntity.ok(productDTO);
+    }
+    // ******************************************************************************************
+
 }
--- src/main/java/com/giulidev/commerce/services/ProductService.java
+++ src/main/java/com/giulidev/commerce/services/ProductService.java
@@ -18,14 +18,15 @@ public class ProductService {
     @Autowired
     private ProductsRepository repository;
 
-    // BUSCAR REGISTROS POR ID E TODOS (PAGINADOS)
+    // BUSCAR REGISTROS 🔎
     // ****************************************************************************************
+    // 🔢 POR ID
     @Transactional(readOnly = true)
     public ProductDTO findByID(Long id) {
         Product product = repository.findById(id).get();
         return new ProductDTO(product);
     }
-
+    // 💯 TODOS (PAGINADOS)
     @Transactional(readOnly = true)
     public Page<ProductDTO> findAll(Pageable pageable) {
         Page<Product> productList = repository.findAll(pageable);
@@ -33,7 +34,7 @@ public class ProductService {
     }
     // ******************************************************************************************
 
-    // CRIANDO NOVO REGISTRO
+    // CRIANDO NOVO REGISTRO 📝
     // ******************************************************************************************
     @Transactional
     public ProductDTO insert(ProductDTO productDTO) {
@@ -42,4 +43,32 @@ public class ProductService {
         return new ProductDTO(product);
     }
     // ******************************************************************************************
+
+    // ATUALIZANDO REGISTRO 🛠️
+    // ******************************************************************************************
+    @Transactional
+    public ProductDTO update(Long id, ProductDTO productDTO) {
+       // crio uma referência do produto pelo ID que veio - preparo o objeto
+        // ele é monitorado pela JPA
+       Product product = repository.getReferenceById(id);
+
+       // altero esse produto com os dados do DTO que veio
+        copyDtoToEntity(product, productDTO);
+
+       // salvo esse novo produto com o objeto referenciado
+       product = repository.save(product);
+
+       // retorno pro controller o DTO alterado
+       return new ProductDTO(product);
+    }
+    // ******************************************************************************************
+
+    // METODO AUXILIAR PARA CONVERSAO DE DTO PARA PRODUCT 🔁
+    private void copyDtoToEntity (Product product, ProductDTO productDTO) {
+        product.setName(productDTO.getName());
+        product.setDescription(productDTO.getDescription());
+        product.setPrice(productDTO.getPrice());
+        product.setImgUrl(productDTO.getImgUrl());
+    }
+
 }
